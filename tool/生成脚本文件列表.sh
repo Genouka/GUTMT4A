97cbeea7cb9fca60b9f40b4e8489f65b4299964d
Genouka <142009020+Genouka@users.noreply.github.com>
2025-09-03 14:04:36 +0800

    更新生成脚本列表的脚本

--- tool/生成脚本文件列表.sh
+++ tool/生成脚本文件列表.sh
@@ -1,1 +1,5 @@
-find ./QiuUTMT/Resources/Raw -type f -printf '%P\n' > scriptpath.txt
+# Copyright by Genouka 2025
+# Notice: The script is licensed under MPL2.0 so that you can use the script in other projects.
+find ../QiuUTMT/Resources/Raw/ -type f \( -name "*.cs" -o -name "*.csx" \) -printf '%P;;' \
+| sed 's/;;$//' \
+> ../QiuUTMT/Resources/Raw/scriptpath.txt
