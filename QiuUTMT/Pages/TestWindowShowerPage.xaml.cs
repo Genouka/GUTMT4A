3802d9f2098fd2ca8fdf16b480cfefdfc7cfc975
Genouka <142009020+Genouka@users.noreply.github.com>
2025-09-03 16:14:22 +0800

    增加测试页面

---
+++ QiuUTMT/Pages/TestWindowShowerPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QiuUTMT.Pages;
+
+public partial class TestWindowShowerPage : ContentPage
+{
+    public TestWindowShowerPage()
+    {
+        InitializeComponent();
+    }
+}
